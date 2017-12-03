e5e455f9a3484d9e2cea1e2d228ce694cb9f8cab
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-12-03 14:28:49 -0500

    Aded upper-footer Edited animation of the side menu

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -49,14 +49,28 @@ animate();
 //**********************
 // The menu on the side
 //**********************
+var menuOn = false;
+
+$("#menu-btn").hover(
+	function() {
+		$("#menu").get(0).style.width = "10px";
+	}, function() {
+		if (!menuOn) {
+			$("#menu").get(0).style.width = "0";
+		}
+	}
+);
+
 $("#menu-btn").click(function() {
-	$("#menu").get(0).style.width = "30%";
+	$("#menu").get(0).style.width = "36%";
 	this.style.display = "none";
+	menuOn = true;
 });
 
 $(".closebtn").click(function() {
 	$("#menu").get(0).style.width = "0";
 	$("#menu-btn").get(0).style.display = "block";
+	menuOn = false;
 });
 
 //**********************
