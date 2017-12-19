f3116a025c01c0ae92a6e8b0a6bfe28c22130ca2
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-12-19 01:13:01 -0500

    Redesigned side menu Refactored some codes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,42 @@
 //**********************
 // The menu on the side
 //**********************
-var menuOn = false;
+var menuOn = false,
+	menu = $("#menu").get(0),
+	menuBtn = $("#menu-btn"),
+	closeBtn = $(".closebtn");
 
-$("#menu-btn").hover(
+function menuResize() {
+	if ($(window).width() <= 767) {
+		menu.style.width = "50%";
+	}
+	else {
+		menu.style.width = "33%";
+	}
+}
+
+menuBtn.hover(
 	function() {
-		$("#menu").get(0).style.width = "10px";
+		menu.style.width = "10px";
 	}, function() {
 		if (!menuOn) {
-			$("#menu").get(0).style.width = "0";
+			menu.style.width = "0";
 		}
 	}
 );
 
-$("#menu-btn").click(function() {
-	$("#menu").get(0).style.width = "33%";
+$(window).resize(function() {
+	if (menuOn) menuResize();
+});
+
+menuBtn.click(function() {
+	menuResize();
 	this.style.display = "none";
 	menuOn = true;
 });
 
-$(".closebtn").click(function() {
-	$("#menu").get(0).style.width = "0";
-	$("#menu-btn").get(0).style.display = "block";
+closeBtn.click(function() {
+	menu.style.width = "0";
+	menuBtn.get(0).style.display = "block";
 	menuOn = false;
 });
