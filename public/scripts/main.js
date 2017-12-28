67b5d561ab889535f3d5b6a9032de7752448cab2
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-12-28 14:47:58 -0400

    Added active state for side menu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,7 +4,12 @@
 var menuOn = false,
 	menu = $("#menu").get(0),
 	menuBtn = $("#menu-btn"),
-	closeBtn = $(".closebtn");
+	closeBtn = $(".closebtn"),
+	homeSide = $("#home-side").get(0),
+	aboutSide = $("#about-side").get(0),
+	projectsSide = $("#projects-side").get(0),
+	contactSide = $("#contact-side").get(0),
+	overlay = $(".overlay-content");
 
 function menuResize() {
 	if ($(window).width() <= 767) {
@@ -14,6 +19,35 @@ function menuResize() {
 		menu.style.width = "33%";
 	}
 }
+
+function removeSideActive() {
+	homeSide.classList.remove("overlay-active");
+	aboutSide.classList.remove("overlay-active");
+	projectsSide.classList.remove("overlay-active");
+	contactSide.classList.remove("overlay-active");
+}
+
+function init() {
+	removeSideActive();	
+	var currentUrl = window.location.href.toString();
+	switch (currentUrl[currentUrl.length - 2]) {
+		case "0":
+			homeSide.classList.add("overlay-active");
+			break;
+		case "u":
+			aboutSide.classList.add("overlay-active");
+			break;
+		case "t":
+			projectsSide.classList.add("overlay-active");
+		case "c":
+			contactSide.classList.add("overlay-active");
+			break;
+		default:
+			break;
+	}
+}
+
+init();
 
 menuBtn.hover(
 	function() {
