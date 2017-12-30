cd4bd153ef8854e93fe5863e513e2b232a101c90
Nhan Nguyen <nhan.nguyen@uwaterloo.ca>
2017-12-30 00:10:36 -0400

    Added start script

--- models/project.js
+++ models/project.js
@@ -4,7 +4,8 @@ var projectSchema = new mongoose.Schema({
 	image: String,
 	category: String,
 	title: String,
-	content: String
+	content: String,
+	url: String
 });
 
 module.exports = mongoose.model("Project", projectSchema);
--- seeds/project.js
+++ seeds/project.js
@@ -6,37 +6,43 @@ var projectData = [
 		image: "../images/RGBGame.png",
 		category: "Web Game",
 		title: "The RGB Game",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen/RGBGame"
 	},
 	{
 		image: "../images/MikkoCompression.png",
-		category: "Offline script",
+		category: "Offline Script",
 		title: "Mikko Compression",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen/MikkoCompression"
 	},
 	{
 		image: "../images/CurrentCConverter.png",
-		category: "Andoird template",
+		category: "Andoird App",
 		title: "CurrentC Converter",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen/CurrentCConverter"
 	},
 	{
 		image: "../images/HushUp.png",
 		category: "Andoird App",
 		title: "Hush Up",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen/HushUp"
 	},
 	{
 		image: "../images/TicTacToe.png",
-		category: "Andoird app",
+		category: "Andoird App",
 		title: "Tic Tac Toe",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen"
 	},
 	{
 		image: "../images/ToDoList.png",
-		category: "Web app",
+		category: "Web App",
 		title: "TO-DO List",
-		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		content: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+		url: "https://github.com/nhan-tri-nguyen/TodoList"
 	}
 ];
 
